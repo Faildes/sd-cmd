9c5c4259df9ad9ee1cf90593e36a6ee6d5c41399
Faildes <127287320+Faildes@users.noreply.github.com>
2023-05-10 10:11:36 +0900

    Update txt2img.py

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -23,7 +23,6 @@ from ldm.util import instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.plms import PLMSSampler
 from ldm.models.diffusion.dpm_solver import DPMSolverSampler
-from ldm.models.diffusion.ddpm import LatentDepth2ImageDiffusion
 
 #from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
 from transformers import AutoFeatureExtractor
